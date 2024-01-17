1d5b2bfba9f6c7516a2f7367dc1d3d5f5dd79ba1
mkhoi15 <khoivmse171254@fpt.edu.vn>
2024-01-17 15:20:02 +0700

    End of Section 6

---
+++ src/AuctionService/Consumers/BidPlaceConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlaceConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+    public BidPlaceConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("---> Consuming BidPlaced Event");
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHightBid == null
+           || context.Message.BidStatus.Contains("Accepted")
+           && context.Message.Amount > auction.CurrentHightBid)
+        {
+            auction.CurrentHightBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedComsumer.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedComsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("---> Consuming BidPlaced Event");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.BidStatus.Contains("Accepted")
+           && context.Message.Amount > auction.CurrentHightBid)
+        {
+            auction.CurrentHightBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
